36d5a1de017f0e2bf82a9dfdb1763b1832874da0
tylercarberry <tylercarberry@gmail.com>
2016-06-04 10:03:21 -0400

    Use firebase database for resources
    Instead of remote config

--- app/src/main/java/com/tytanapps/ptsd/fragments/ResourcesFragment.java
+++ app/src/main/java/com/tytanapps/ptsd/fragments/ResourcesFragment.java
@@ -2,20 +2,27 @@ package com.tytanapps.ptsd.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tytanapps.ptsd.R;
-import com.tytanapps.ptsd.RemoteConfigurable;
 
 
 /**
  * Shows information about PTSD. Gives symptoms, causes, and treatment for PTSD.
  */
 public class ResourcesFragment extends Fragment {
+
+    private static final String LOG_TAG = ResourcesFragment.class.getSimpleName();
 
     public ResourcesFragment() {
         // Required empty public constructor
@@ -26,23 +33,97 @@ public class ResourcesFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_resources, container, false);
 
-        if(getActivity() instanceof RemoteConfigurable) {
-            FirebaseRemoteConfig remoteConfig = ((RemoteConfigurable)getActivity()).getRemoteConfig();
-
-            updateRemoteText(remoteConfig, (TextView) rootView.findViewById(R.id.symptoms_textview), "symptoms_text");
-            updateRemoteText(remoteConfig, (TextView) rootView.findViewById(R.id.causes_textview), "causes_text");
-            updateRemoteText(remoteConfig, (TextView) rootView.findViewById(R.id.diagnosis_textview), "diagnosis_text");
-            updateRemoteText(remoteConfig, (TextView) rootView.findViewById(R.id.treatment_textview), "treatment_text");
-            updateRemoteText(remoteConfig, (TextView) rootView.findViewById(R.id.counseling_textview), "counseling_text");
-        }
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        readResources(database);
 
         return rootView;
     }
 
-    private void updateRemoteText(FirebaseRemoteConfig remoteConfig, TextView textView, String key) {
-        String text = remoteConfig.getString(key);
-        text = text.replaceAll("%", "\n");
-        textView.setText(text);
+    private void readResources(final FirebaseDatabase database) {
+        DatabaseReference myRef = database.getReference("resources");
+
+        // Read from the database
+        myRef.orderByChild("order").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        View rootView = getView();
+                        if(rootView != null) {
+
+                            LinearLayout phoneNumbersLinearLayout = (LinearLayout) rootView.findViewById(R.id.resources_linear_layout);
+                            LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+                            for (DataSnapshot child : dataSnapshot.getChildren()) {
+                                insertResource(child, phoneNumbersLinearLayout, inflater);
+                            }
+                        }
+                    }
+                });
+
+                t.run();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(LOG_TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    private void insertResource(final DataSnapshot phoneDataSnapshot, final LinearLayout phoneNumbersLinearLayout, final LayoutInflater inflater) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String title = (String) phoneDataSnapshot.child("title").getValue();
+                String desc = (String) phoneDataSnapshot.child("description").getValue();
+
+                LinearLayout resourceHeaderView = getResourceView(inflater, phoneNumbersLinearLayout, title);
+
+                TextView headerTextView = (TextView) resourceHeaderView.findViewById(R.id.resource_header);
+                headerTextView.setText(title);
+
+                TextView descTextView = (TextView) resourceHeaderView.findViewById(R.id.resource_desc);
+                descTextView.setText(desc);
+            }
+        });
+
+        t.run();
+    }
+
+    private LinearLayout getResourceView(LayoutInflater inflater, LinearLayout resourcesLinearLayout, String name) {
+        Log.d(LOG_TAG, "getResourceView() called with: " + "inflater = [" + inflater + "], resourcesLinearLayout = [" + resourcesLinearLayout + "], name = [" + name + "]");
+
+        LinearLayout resourceView = null;
+
+        for(int i = 0; i < resourcesLinearLayout.getChildCount(); i++) {
+            View child = resourcesLinearLayout.getChildAt(i);
+
+            if(child.getTag() != null && child.getTag().equals(name))
+                resourceView = (LinearLayout) child;
+        }
+        if(resourceView == null) {
+            resourceView = (LinearLayout) inflater.inflate(R.layout.resource_item, (ViewGroup) getView(), false);
+
+            resourceView.setTag(name);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            params.setMargins(0, 0, 0, (int) getResources().getDimension(R.dimen.activity_vertical_margin));
+            resourceView.setLayoutParams(params);
+
+            resourcesLinearLayout.addView(resourceView);
+        }
+
+        return resourceView;
     }
 
 }
